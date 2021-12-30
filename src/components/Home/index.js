447e428bcc0505d6df22901f48848c44850e8849
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2021-12-30 18:49:59 -0400

    iniciando home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,19 @@
 import './styles.css'
+import icon_logo from '../../assets/icon_pomodoro.svg'
 
 export default function Home(){
     return(
         <div className='home-app'>
 
-            <h2>Status</h2>
+            <h2 className="statusPomodoro">Status</h2>
 
-            <img src='#'></img>
+            <img src={icon_logo}></img>
+
+            <span className="clockPomodoro">00:00</span>
+
+            <div className='buttonStatus'>
+                <button className='btn'></button>
+            </div>
 
             
 
